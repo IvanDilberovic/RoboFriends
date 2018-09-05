373bd0bf031c3b2bdb51ac0a9ec32fc1204c36c8
IvanDilberovic <dilberovic.ivan@gmail.com>
2018-09-05 20:44:05 +0200

    Adding Redux for state monitoring and Redux logger for change state logging.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css'
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchField
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return{
+	 onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
 
 class App extends Component {
 
 	constructor(){
 		super();
 		this.state = {
-			robots: [],
-			searchfield: ''
+			robots: []
 		}
 	}
 
@@ -20,20 +33,19 @@ class App extends Component {
 			.then(response=> response.json())			
 			.then(users=>this.setState({robots: users}));
 			}
-	
-
-	onSearchChange = (event) => {		
-		this.setState({searchfield: event.target.value});		
-	}
 
 	render(){
-		const filteredRobots = this.state.robots.filter(robot => {
-			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		const { robots } = this.state;
+		const { searchField, onSearchChange} = this.props;
+
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		})
+		
 		return(
 			<div className='tc'>
 			<h1 className='f1'>RoboFriends</h1>
-			<SearchBox searchChange={this.onSearchChange}/>
+			<SearchBox searchChange={onSearchChange}/>
 			<Scroll>
 				<ErrorBoundry>
 					<CardList robots={filteredRobots}/>			
@@ -44,4 +56,4 @@ class App extends Component {
 	}	
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
- import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import { searchRobots } from './reducers';
 
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+					<App />
+				</Provider>,document.getElementById('root'));
 registerServiceWorker();
